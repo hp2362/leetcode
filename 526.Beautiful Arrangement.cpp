79d12b40006edf466f55330af42c17ee9ea16131
ouyj <964358093@qq.com>
2017-08-09 14:32:13 +0800

    526.Beautiful Arrangement.cpp

--- 526.Beautiful Arrangement.cpp
+++ 526.Beautiful Arrangement.cpp
@@ -5,6 +5,21 @@ The number at the ith position is divisible by i.
 i is divisible by the number at the ith position.
 Now given N, how many beautiful arrangements can you construct?
 
+Input: 2
+Output: 2
+Explanation:
+
+The first beautiful arrangement is [1, 2]:
+
+Number at the 1st position (i=1) is 1, and 1 is divisible by i (i=1).
+
+Number at the 2nd position (i=2) is 2, and 2 is divisible by i (i=2).
+
+The second beautiful arrangement is [2, 1]:
+
+Number at the 1st position (i=1) is 2, and 2 is divisible by i (i=1).
+
+Number at the 2nd position (i=2) is 1, and i (i=2) is divisible by 1.
 */
 
 class Solution {
